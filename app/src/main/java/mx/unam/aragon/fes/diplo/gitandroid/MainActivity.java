84ba2a25f8b9d2d4f3eb0727bba396f27d47c2c8
RogueFenrir <blitzkrieg_z5@mail.com>
2018-02-10 13:35:29 -0600

    Boton nuevo y funcionalidad

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -2,12 +2,35 @@ package mx.unam.aragon.fes.diplo.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton1;
+    private TextView textview1;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton1=findViewById(R.id.button);
+        textview1=findViewById(R.id.textView);
+
+        boton1.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                textview1.setText("Sieg Heil");
+            }
+
+
+        });
+
     }
 }
